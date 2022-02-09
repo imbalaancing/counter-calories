e463308abc0edd2b496f867bacc766ec923dd4ec
Nick K <nikkalaancing@gmail.com>
2022-02-09 12:54:48 +0700

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 const parameterAge = document.getElementById('age');
 const parameterHeight = document.getElementById('height');
 const parameterWeight = document.getElementById('weight');
-
 const button = document.getElementById('btn');
 
-const activateButton = () => {
+parameterAge.addEventListener('keyup', activateButton);
+parameterHeight.addEventListener('keyup', activateButton);
+parameterWeight.addEventListener('keyup', activateButton);
+
+function activateButton() {
     if (parameterAge.value === '' || parameterHeight.value === '' || parameterWeight.value === '') {
         return button.setAttribute('disabled', '');
+    } else {
+        return button.removeAttribute('disabled'); 
     }
-    return button.removeAttribute('disabled');
 }
 
-parameterAge.onkeyup = activateButton;
-parameterHeight.onkeyup = activateButton;
-parameterWeight.onkeyup = activateButton;
 
+// parameterAge.onkeyup = activateButton;
+// parameterHeight.onkeyup = activateButton;
+// parameterWeight.onkeyup = activateButton;
+
+// const activateButton = () => {
+//     if (parameterAge.value === '' || parameterHeight.value === '' || parameterWeight.value === '') {
+//         button.setAttribute('disabled', '');
+//     } else {
+//         button.removeAttribute('disabled'); 
+//     }
+// }
