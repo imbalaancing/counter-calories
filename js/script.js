5feca6b10b5fb119c07f153a359ee0d6a118ded6
Nick K <nikkalaancing@gmail.com>
2022-02-09 13:47:55 +0700

    add reset button

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
+// "calculate" button and "clear fields and calculation" button
 const parameterAge = document.getElementById('age');
 const parameterHeight = document.getElementById('height');
 const parameterWeight = document.getElementById('weight');
-const button = document.getElementById('btn');
 
-parameterAge.addEventListener('keyup', activateButton);
-parameterHeight.addEventListener('keyup', activateButton);
-parameterWeight.addEventListener('keyup', activateButton);
+const buttonCalculate = document.getElementById('btn');
+const buttonReset = document.querySelector('.form__reset-button');
 
-function activateButton() {
+parameterAge.addEventListener('keyup', activateCalcButton);
+parameterHeight.addEventListener('keyup', activateCalcButton);
+parameterWeight.addEventListener('keyup', activateCalcButton);
+
+function activateCalcButton() {
     if (parameterAge.value === '' || parameterHeight.value === '' || parameterWeight.value === '') {
-        return button.setAttribute('disabled', '');
+        return buttonCalculate.setAttribute('disabled', '');
     } else {
-        return button.removeAttribute('disabled'); 
+        return buttonCalculate.removeAttribute('disabled'); 
     }
-}
+};
 
+parameterAge.addEventListener('keyup', activateResetButton);
+parameterHeight.addEventListener('keyup', activateResetButton);
+parameterWeight.addEventListener('keyup', activateResetButton);
 
-// parameterAge.onkeyup = activateButton;
-// parameterHeight.onkeyup = activateButton;
-// parameterWeight.onkeyup = activateButton;
-
-// const activateButton = () => {
-//     if (parameterAge.value === '' || parameterHeight.value === '' || parameterWeight.value === '') {
-//         button.setAttribute('disabled', '');
-//     } else {
-//         button.removeAttribute('disabled'); 
-//     }
-// }
+function activateResetButton() {
+    if (parameterAge.value === '' && parameterHeight.value === '' && parameterWeight.value === '') {
+        return buttonReset.setAttribute('disabled', '');
+    } else {
+        return buttonReset.removeAttribute('disabled'); 
+    }
+};
